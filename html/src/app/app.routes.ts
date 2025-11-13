c30f90a171bf6170904ff89fd701e49a05b524c7
Szalontai-Laszlo <szalilaci10@gmail.com>
2025-11-13 12:26:48 +0100

    sidebar + docs konyvtar

--- html/src/app/app.routes.ts
+++ html/src/app/app.routes.ts
@@ -2,9 +2,15 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Books } from './pages/books/books';
 import { Loan } from './pages/loan/loan';
+import { AboutUs } from './pages/about-us/about-us';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
 
 export const routes: Routes = [
     {path: '', component:Home},
     {path: 'books', component: Books},
-    {path: 'loan', component: Loan}
+    {path: 'loan', component: Loan},
+    {path: 'about_us', component: AboutUs},
+    {path: 'login', component: Login},
+    {path: 'register', component: Register}
 ];
