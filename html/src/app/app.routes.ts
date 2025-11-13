b506d581698771b57a570877e75d88bdddd4e6e8
Szalontai-Laszlo <szalilaci10@gmail.com>
2025-11-13 09:45:54 +0100

    Előkészítés

--- html/src/app/app.routes.ts
+++ html/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Books } from './pages/books/books';
+import { Loan } from './pages/loan/loan';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:Home},
+    {path: 'books', component: Books},
+    {path: 'loan', component: Loan}
+];
