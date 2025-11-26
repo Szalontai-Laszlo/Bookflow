2442daf709ee3e8dfb9841c86d60fce858d0504b
Szalontai-Laszlo <szalilaci10@gmail.com>
2025-11-26 09:45:49 +0100

    Home HTML alapok

--- html/src/app/components/sidebar/sidebar.ts
+++ html/src/app/components/sidebar/sidebar.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faBook,faBookOpen,faHome,faBookmark,faUsers,faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { faBook,faBookOpen,faHome,faBookmark,faUsers,faArrowLeft,faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons';
 import { SidebarItem } from '../../interfaces/sidebar.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -17,9 +18,10 @@ export class Sidebar {
   readonly faBookmark = faBookmark;
   readonly faUsers = faUsers;
   readonly faArrowLeft = faArrowLeft;
+  readonly faArrowRightToBracket = faArrowRightToBracket;
 
   isLeftNavOpen = signal<boolean>(true);
-
+  constructor(private router: Router) {}
   navItems: SidebarItem[] = [
     {
       icon: this.faHome,
@@ -32,21 +34,29 @@ export class Sidebar {
       icon: this.faBookOpen,
       label: 'Könyvtár',
       route: '/books',
-      isActive: true
+      isActive: false
     },
 
     {
       icon: this.faBookmark,
       label: 'Kölcsönzés',
       route: '/loan',
-      isActive: true
+      isActive: false
     },
 
     {
       icon: this.faUsers,
       label: 'Rólunk',
       route: '/about_us',
-      isActive: true
+      isActive: false
     },
   ];
+
+  activeRoute(item: SidebarItem, index: number): void {
+    this.navItems.map((navItem, i) => {
+      navItem.isActive = i === index;
+
+      this.router.navigateByUrl(item.route);
+    });
+  }
 }
--- html/src/app/pages/home/home.ts
+++ html/src/app/pages/home/home.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import {
+  faSun,
+  faMoon
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   imports: [],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
+  // Iconok a dark/light váltóhoz
+  faSun = faSun;
+  faMoon = faMoon;
 
+  // Friss könyvek blokk
+  latestBooks = [
+    {
+      title: 'Dűne – A próféta',
+      author: 'Frank Herbert',
+      img: 'assets/books/dune2.jpg'
+    },
+    {
+      title: 'A király visszatér',
+      author: 'J. R. R. Tolkien',
+      img: 'assets/books/lotr3.jpg'
+    },
+    {
+      title: 'Alapítvány és Föld',
+      author: 'Isaac Asimov',
+      img: 'assets/books/foundation5.jpg'
+    },
+    {
+      title: 'Hyperion',
+      author: 'Dan Simmons',
+      img: 'assets/books/hyperion.jpg'
+    },
+    {
+      title: '1984',
+      author: 'George Orwell',
+      img: 'assets/books/1984.jpg'
+    }
+  ];
 }
