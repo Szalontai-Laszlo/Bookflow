02fa2c122814732cbc46e9d7ea7d8c9a2ddf316f
TothLaszloGabor <lacikaaapro@gmail.com>
2025-12-10 21:53:07 +0100

    a képek megjelenitése a home oldalon

--- html/src/app/pages/home/home.ts
+++ html/src/app/pages/home/home.ts
@@ -21,27 +21,27 @@ export class Home {
     {
       title: 'Dűne – A próféta',
       author: 'Frank Herbert',
-      img: 'assets/books/dune2.jpg'
+      img: 'assets/books/duneaprofeta.png'
     },
     {
       title: 'A király visszatér',
       author: 'J. R. R. Tolkien',
-      img: 'assets/books/lotr3.jpg'
+      img: 'assets/books/akiralyvisszater.png'
     },
     {
       title: 'Alapítvány és Föld',
       author: 'Isaac Asimov',
-      img: 'assets/books/foundation5.jpg'
+      img: 'assets/books/alapitvanyesfold.png'
     },
     {
       title: 'Hyperion',
       author: 'Dan Simmons',
-      img: 'assets/books/hyperion.jpg'
+      img: 'assets/books/hyperion.png'
     },
     {
       title: '1984',
       author: 'George Orwell',
-      img: 'assets/books/1984.jpg'
+      img: 'assets/books/1984.png'
     }
   ];
 }
