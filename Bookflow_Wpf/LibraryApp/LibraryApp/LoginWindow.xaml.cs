8076ef0a439794ee04dc0be8b17a78ef307cc01d
TothLaszloGabor <lacikaaapro@gmail.com>
2025-12-11 12:29:34 +0100

    WPF: LoginWindow bejelentkezés fül programozásának megkezdése, Főoldal: Javítások a books.html-ben

--- Bookflow_Wpf/LibraryApp/LibraryApp/LoginWindow.xaml.cs
+++ Bookflow_Wpf/LibraryApp/LibraryApp/LoginWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,14 +21,37 @@ namespace LibraryApp
     /// </summary>
     public partial class LoginWindow : Window
     {
+        MySqlConnection connection = new MySqlConnection("server=localhost; database=bookflow; uid=root; charset=utf8mb4");
+        string query = "";
         public LoginWindow()
         {
             InitializeComponent();
         }
-
+        private void Open_Connection()
+        {
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+        private void Close_Connection()
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
         private void button_Kilepes_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            query = "SELECT `email`, `password`, `type` FROM `users`";
+            MySqlDataAdapter adapter = new MySqlDataAdapter(query, connection); 
+            DataTable dt = new DataTable();
+            if (dt[0] == textBox_Email.)
         }
     }
 }
