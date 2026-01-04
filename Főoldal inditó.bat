ffdd289fc7df7a886bec52fe666adaf1d9d5a5e2
TothLaszloGabor <lacikaaapro@gmail.com>
2026-01-04 20:40:15 +0100

    Inditó bat és teendők irása

---
+++ Főoldal inditó.bat
@@ -0,0 +1,22 @@
+@echo off
+echo Angular és Express szerver indítása egy ablakban...
+
+REM --- Angular szerver indítása háttérben, sárga színnel ---
+cd html
+start /b powershell -command "npx ng serve 2>&1 | ForEach-Object { Write-Host '[ANGULAR]: ' -ForegroundColor Yellow -NoNewline; Write-Host $_ }"
+cd ..
+
+REM --- Express szerver indítása háttérben, zöld színnel ---
+cd html\server
+start /b powershell -command "node server.js 2>&1 | ForEach-Object { Write-Host '[EXPRESS]: ' -ForegroundColor Green -NoNewline; Write-Host $_ }"
+cd ..\..
+
+echo.
+echo ================================
+echo Angular fut: http://localhost:4200
+echo Express fut: http://localhost:3000
+echo ================================
+echo.
+
+echo A szerverek futnak. Az ablak nyitva marad.
+pause
--- Teendők.md
+++ Teendők.md
@@ -1,6 +1,6 @@
 # Főoldal (Angular):
     - Kölcsönzések oldal csak akkor jelenüljön meg mikor bevan jelentkezve a felhasználó
-    - Adatbázisból menjen az egész Home, Könyvtár ból a képek és a cimek
+    - Adatbázisból menjen az egész Home, Könyvtár ból a képek és a cimek ✅
 # Admin felület (WPF):
     - AddBookWindowon belül adatbázisba felmenjen az adott könyv
     - RendBookWindow és a MainWindowon a Szerkeztés és a Törlés gomb müködése
